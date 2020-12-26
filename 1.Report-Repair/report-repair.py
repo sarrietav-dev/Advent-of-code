40d9e822ae0bdb9a8ff420d09271c0c5ebea8be1
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-12-25 23:09:21 -0500

    Added password philosophy

--- 1.Report-Repair/report-repair.py
+++ 1.Report-Repair/report-repair.py
@@ -11,5 +11,6 @@ def get_answer(num_combinations):
     result = list(filter(lambda nums: sum(nums) == 2020, comb))[0]
     return functools.reduce(operator.mul, result)
 
-print(f"Part 1: {get_answer(2)}")
-print(f"Part 2: {get_answer(3)}")
+
+print(f"part 1: {get_answer(2)}")
+print(f"part 2: {get_answer(3)}")
